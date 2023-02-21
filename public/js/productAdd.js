e7bbb0034c09ddd623978b9e5af0c9f7fc8c9988
Migggzz <miggz.gommz@gmail.com>
2023-02-20 21:51:25 -0600

    se agrego funcionalidad de websockets

--- public/js/productAdd.js
+++ public/js/productAdd.js
@@ -5,12 +5,17 @@ const timeString = now.toLocaleTimeString();
 
 alert('hello world');
 
-// socket.on('logs', data => {
-//     console.log(data)
-//     const divLog = document.getElementById('messageLogs');
-//     let messages = '';
-//     data.reverse().forEach(element => {
-//         messages += `<p><i> ${element.user}</i>: ${timeString} : ${element.message}</p>`
-//     });
-//     divLog.innerHTML = messages
-// })
+socket.on('logs', data => {
+    console.log(data)
+    const divLog = document.getElementById('messageLogs');
+    let messages = '';
+    data.forEach(element => {
+        messages += `<p>${element.title}</p>
+                    <p>${element.price}</p>`
+    });
+    divLog.innerHTML = messages
+})
+
+doSubmit = (e) => {
+    socket.emit('message', {user, message: e.target.value, timeString})
+}
--- src/app.js
+++ src/app.js
@@ -13,16 +13,18 @@ const cartsFile = "carts.json";
 
 const app = express();
 
+let products = productsFile || [];
+
 // app.listen(8080),() => {
 //     console.log('Server is running on port 8080')
 // }
 
 const httpServer = app.listen(PORT, () => console.log('Server started'));
-// console.log(productsFile);
+console.log(productsFile);
 const io = new Server(httpServer);
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/view');
 app.set('view engine', 'handlebars');
@@ -36,12 +38,26 @@ app.use('/', viewProductsRouter);
 
 app.use('/realtimeproducts', realTimeRouter);
 
-io.on('connection', socket => {
-    console.log('New client connected!')
-    io.emit('logs', productsFile)
-    socket.on('message', data => {
-        console.log(data)
-        io.emit('logs', messages)
-    })
-})
-
+// io.on('connection', socket => {
+//     console.log('New client connected!')
+//     io.emit('logs', productsFile)
+//     socket.on('message', data => {
+//         console.log(data)
+//         io.emit('logs', messages)
+//     })
+// })
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    socket.on('addProduct', (product) => {
+    console.log(product)
+      console.log(`Adding product ${product.title} with price ${product.price}`);
+      products.push({...product, id: products.length + 1});
+      console.log(products);
+      io.emit('productAdded', products);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
