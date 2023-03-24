a533dc66c57e66fa05495dc38710b78dd6005e34
Migggzz <miggz.gommz@gmail.com>
2023-03-24 13:33:27 -0600

    added info from ent-2

---
+++ src/models/cart.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const cartCollection = "carts"
+
+const cartSchema = new mongoose.Schema({
+    products: {
+        type: [{
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "products"
+            },
+            quantity: Number
+        }],
+        default: []
+    }
+})
+
+mongoose.set("strictQuery", false)
+const cartModel = mongoose.model(cartCollection, cartSchema)
